7c08b5478f653d0debb9328d044762d12ee6a4a1
Kevinp9725 <86507034+Kevinp9725@users.noreply.github.com>
2024-04-27 15:24:06 -0600

    Update movie.js

--- schema/movie.js
+++ schema/movie.js
@@ -16,23 +16,4 @@ const agregarDatos = (pelicula) => {
 
 module.exports = {
   agregarDatos
-=======
-const z = require('zod')
-
-const agregar = z.object({
-    title: z.string(),
-    year: z.number().int().min(1000).max(9999),
-    director: z.string(),
-    duration: z.number().positive(),
-    poster: z.string().url(),
-    genre: z.array(z.string()),
-    rate: z.number()
-  })
-
-const agregarDatos = (pelicula) => {
-  return agregar.safeParse(pelicula)
-} 
-
-module.exports = {
-  agregarDatos
 }
